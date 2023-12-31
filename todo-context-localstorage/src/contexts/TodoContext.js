ee5732ae4268a3cdb7a7439af8bffb540e43e564
AyushP11 <ayushpanchakshari@gmail.com>
2023-12-31 16:11:57 +0530

    todo-context using local storage done

---
+++ todo-context-localstorage/src/contexts/TodoContext.js
@@ -0,0 +1,24 @@
+
+import { useContext, createContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: "1",
+      todo: "todo msg",
+      completed: false,
+    }
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+})
+
+export const TodoProvider = TodoContext.Provider;
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+} 
+
+
